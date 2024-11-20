ba9570319545199cc667b654b143c4ec3c1cf60a
shafiq <ssekittoshafiq@gmail.com>
2024-11-20 17:43:12 +0300

    yty

---
+++ enrollment/views.py
@@ -0,0 +1,261 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.contrib import messages
+from xhtml2pdf import pisa
+from io import BytesIO
+from .models import *
+from .forms import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from accounts.decorators import admin_required
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from io import BytesIO
+
+
+from .filters import SchoolEnrollmentFilter  # Assume you have created this filter
+
+
+def SchoolEnrollments(request):
+    # Get school from user profile
+    school = request.user.profile  # Assuming profile has school attribute
+
+    # Get all enrollments for the school
+    enrollments = SchoolEnrollment.objects.filter(school=school)
+
+    if request.method == "POST":
+        form = SchoolEnrollmentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            enrollment = form.save(commit=False)
+            enrollment.school = school
+            enrollment.save()
+            messages.success(request, "Enrollment created successfully!")
+            return redirect("school_enrollments")
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = SchoolEnrollmentForm()
+
+    context = {"form": form, "enrollments": enrollments}
+    return render(request, "enrollments/school_enrolls.html", context)
+
+
+def AllEnrollments(request):
+    # Get all school_enrolls
+    school_enrolls = SchoolEnrollment.objects.all()
+
+    # Apply the filter
+    # school_enroll_filter = SchoolEnrollmentFilter(request.GET, queryset=school_enrolls)
+    # filtered_school_enrolls = school_enroll_filter.qs
+
+    # Prepare context with athletes data
+    context = {
+        "school_enrolls": school_enrolls,
+    }
+
+    return render(request, "enrollments/enroll.html", context)
+
+
+def remove_athlete(request, enrollment_id, athlete_id):
+    athlete_enrollment = get_object_or_404(AthleteEnrollment, id=enrollment_id)
+    athlete = get_object_or_404(Athlete, id=athlete_id)
+
+    if request.method == "POST":
+        athlete_enrollment.athletes.remove(athlete)
+        return HttpResponseRedirect(
+            reverse("school_enrollment", args=[athlete_enrollment.school_enrollment.id])
+        )
+
+    return redirect(
+        "enrollments/school_enrollment", id=athlete_enrollment.school_enrollment.id
+    )
+
+
+def school_enrollment_details(request, id):
+    school_enrollment = get_object_or_404(SchoolEnrollment, id=id)
+    school = school_enrollment.school
+    if request.method == "POST":
+        form = AthleteEnrollmentForm(request.POST)
+        if form.is_valid():
+            athlete_enrollment = AthleteEnrollment.objects.create(
+                school_enrollment=school_enrollment
+            )
+            athlete_enrollment.athletes.set(form.cleaned_data["athletes"])
+            return HttpResponseRedirect(reverse("school_enrollment", args=[id]))
+    else:
+        form = AthleteEnrollmentForm()
+
+    athlete_enrollments = AthleteEnrollment.objects.filter(
+        school_enrollment=school_enrollment
+    )
+    all_athletes = Athlete.objects.filter(school=school)
+
+    context = {
+        "school_enrollment": school_enrollment,
+        "form": form,
+        "athlete_enrollments": athlete_enrollments,
+        "all_athletes": all_athletes,
+    }
+    return render(request, "enrollments/school_enroll.html", context)
+
+
+def school_enrollment_update(request, id):
+    school_enrollment = get_object_or_404(SchoolEnrollment, id=id)
+
+    if request.method == "POST":
+        form = SchoolEnrollmentForm(
+            request.POST, request.FILES, instance=school_enrollment
+        )
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "SchoolEnrollment information updated successfully!"
+            )
+            return redirect("school_enrollment", id=school_enrollment.id)
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = SchoolEnrollmentForm(instance=school_enrollment)
+
+    context = {
+        "form": form,
+        "school_enrollment": school_enrollment,
+    }
+    return render(request, "enrollments/update_school_enroll.html", context)
+
+
+def school_enroll_delete(request, id):
+    stud = SchoolEnrollment.objects.get(id=id)
+    if request.method == "POST":
+        stud.delete()
+        return redirect("school_enrollments")
+
+    return render(request, "enrollments/delete_school_enroll.html", {"obj": stud})
+
+
+from django.shortcuts import render
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.http import HttpResponse
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from io import BytesIO
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+from xhtml2pdf import pisa
+
+# Make sure to import your models
+
+admin_required
+def Accreditation(request, id):
+    team = get_object_or_404(SchoolEnrollment, id=id)
+    athlete_enrollments = AthleteEnrollment.objects.filter(school_enrollment=team)
+    athletes = Athlete.objects.filter(athleteenrollment__in=athlete_enrollments)
+
+    # Get template
+    template = get_template("reports/acred.html")
+
+    # Compress and fix rotation for athletes' photos
+
+    # Prepare context
+    context = {
+        "athletes": athletes,
+        "MEDIA_URL": settings.MEDIA_URL,
+    }
+
+    # Render HTML
+    html = template.render(context)
+
+    # Create a PDF
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = 'attachment; filename="Accreditation.pdf"'
+
+    # Generate PDF from HTML
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    if pisa_status.err:
+        return HttpResponse("We had some errors <pre>" + html + "</pre>")
+
+    return response
+
+
+def Albums(request, id):
+    team = get_object_or_404(SchoolEnrollment, id=id)
+    athlete_enrollments = AthleteEnrollment.objects.filter(school_enrollment=team)
+    athletes = Athlete.objects.filter(athleteenrollment__in=athlete_enrollments)
+    school = team.school
+    officials = school_official.objects.filter(school=school)
+
+    # Get athlete and official counts
+    athlete_count = athletes.count()
+    official_count = officials.count()
+
+    # Create a unique filename
+    filename = f"{team.school} | {team.sport} .pdf"
+
+    # Get template
+    template = get_template("reports/albums.html")
+
+    # Prepare context
+    context = {
+        "team": team,
+        "official_count": official_count,
+        "athlete_count": athlete_count,
+        "athletes": athletes,
+        "officials": officials,
+        "MEDIA_URL": settings.MEDIA_URL,
+    }
+
+    # Render HTML
+    html = template.render(context)
+
+    # Create a PDF
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f'attachment; filename="{filename}"'
+
+    # Generate PDF from HTML
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    if pisa_status.err:
+        return HttpResponse("We had some errors <pre>" + html + "</pre>")
+
+    return response
+
+admin_required
+def Certificate(request, id):
+    team = get_object_or_404(SchoolEnrollment, id=id)
+    athlete_enrollments = AthleteEnrollment.objects.filter(school_enrollment=team)
+    athletes = Athlete.objects.filter(athleteenrollment__in=athlete_enrollments)
+
+    # Get template
+    template = get_template("reports/cert.html")
+
+    # Compress and fix rotation for athletes' photos
+
+    # Prepare context
+    context = {
+        "athletes": athletes,
+        "MEDIA_URL": settings.MEDIA_URL,
+    }
+
+    # Render HTML
+    html = template.render(context)
+
+    # Create a PDF
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = 'attachment; filename="Accreditation.pdf"'
+
+    # Generate PDF from HTML
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    if pisa_status.err:
+        return HttpResponse("We had some errors <pre>" + html + "</pre>")
+
+    return response
---
+++ transfers/views.py
@@ -0,0 +1,208 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
+from .models import TransferRequest
+from school.models import *
+from .forms import *
+from django.utils import timezone
+
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+# from accounts.decorators import transfer_required
+
+
+# @transfer_required
+def approve_transfer(request, transfer_id):
+    transfer_request = get_object_or_404(TransferRequest, id=transfer_id)
+
+    # Check if the user has the right permissions (e.g., if they are the approver)
+    if (
+        request.user == transfer_request.approver
+        and transfer_request.status == "accepted"
+    ):
+        transfer_request.approve_transfer()
+        messages.success(
+            request,
+            "Transfer approved and athlete has been transferred to the new school.",
+        )
+    else:
+        messages.error(
+            request,
+            "You are not authorized to approve this transfer or it is not ready for approval.",
+        )
+
+    return redirect("some_view_name")
+
+
+# Assume you create a form for transfer request
+
+
+def transfers(request):
+    user = request.user
+    school = user.school
+    athletes = Athlete.objects.all().exclude(school=school)
+    context = {"athletes": athletes}
+    return render(request, "transfers/initiate_transfer.html", context)
+
+
+def initiate_transfer(request, id):
+    try:
+        user = request.user
+        school = user.school  # The requesting school
+        athlete = get_object_or_404(Athlete, id=id)
+
+        # Prevent transfer request if athlete is already in requested school
+        if athlete.school == school:
+            messages.error(
+                request, "Cannot request transfer for athlete already in your school."
+            )
+            return redirect("athletes_list")
+
+        # Check if there's already a pending transfer request
+        existing_request = TransferRequest.objects.filter(
+            athlete=athlete, status="pending"
+        ).exists()
+
+        if existing_request:
+            messages.error(
+                request, "There is already a pending transfer request for this athlete."
+            )
+            return redirect("mytransfers")
+
+        # Create the transfer request
+        transfer_request = TransferRequest.objects.create(
+            requester=school,
+            athlete=athlete,
+            owner=athlete.school,
+            status="pending",
+            requested_at=timezone.now(),
+        )
+
+        messages.success(request, "Transfer request initiated successfully.")
+        return redirect("mytransfers")
+
+    except Exception as e:
+        messages.error(request, f"An error occurred: {str(e)}")
+        return redirect("transfers")
+
+
+def myTransfers(request):
+    user = request.user
+    school = user.school
+    mytransfers = TransferRequest.objects.filter(requester=school)
+    context = {"mytransfers": mytransfers}
+    return render(request, "transfers/my_transfers.html", context)
+
+
+# @transfer_required
+def transfer_details(request, id):
+    transfer = get_object_or_404(TransferRequest, id=id)
+
+    # Check if user has permission to view this transfer
+    if not request.user.has_perm("transfers.approve_transfer"):
+        messages.error(request, "You don't have permission to view this transfer.")
+        return redirect("home")
+
+    context = {
+        "transfer": transfer,
+    }
+
+    return render(request, "transfers/transfer.html", context)
+
+
+def myRequests(request):
+    user = request.user
+    school = user.school
+    mytransfers = TransferRequest.objects.filter(owner=school, status="pending")
+    context = {"mytransfers": mytransfers}
+    return render(request, "transfers/my_requests.html", context)
+
+
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
+
+def accept_transfer(request, id):
+    try:
+        transfer_request = get_object_or_404(TransferRequest, id=id)
+
+        # Ensure only the owning school can accept the transfer
+        if transfer_request.owner != request.user.school:
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": "You are not authorized to accept this transfer.",
+                }
+            )
+
+        if transfer_request.status != "pending":
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": "This transfer request is no longer pending.",
+                }
+            )
+
+        form = AcceptTransferForm(
+            request.POST, request.FILES, instance=transfer_request
+        )
+        if form.is_valid():
+            transfer_request = form.save(commit=False)
+            transfer_request.accept_transfer()
+            return JsonResponse(
+                {
+                    "status": "success",
+                    "message": "Transfer request accepted successfully.",
+                }
+            )
+        else:
+            return JsonResponse(
+                {
+                    "status": "error",
+                    "message": "Please upload valid transfer documents.",
+                }
+            )
+
+    except Exception as e:
+        print(f"Error in accept_transfer: {str(e)}")  # For debugging
+        return JsonResponse(
+            {
+                "status": "error",
+                "message": "An error occurred while processing your request.",
+            }
+        )
+
+
+@login_required
+def approve_transfer(request, id):
+    try:
+        transfer_request = get_object_or_404(TransferRequest, id=id)
+
+        # Validate if user has permission to approve transfers
+        if not request.user.has_perm("transfers.approve_transfer"):
+            messages.error(request, "You don't have permission to approve transfers.")
+            return redirect("alltransfers")
+
+        # Check if transfer is in accepted state
+        if transfer_request.status != "accepted":
+            messages.error(request, "Only accepted transfers can be approved.")
+            return redirect("alltransfers")
+
+        # Check if documents were uploaded
+        if not transfer_request.documents:
+            messages.error(request, "Transfer cannot be approved without documents.")
+            return redirect("alltransfers")
+
+        # Approve the transfer
+        transfer_request.approve_transfer(approver=request.user)
+        messages.success(
+            request,
+            f"Transfer approved successfully. {transfer_request.athlete} has been transferred to {transfer_request.requester}",
+        )
+
+        return redirect("alltransfers")
+
+    except Exception as e:
+        messages.error(
+            request, f"An error occurred while approving the transfer: {str(e)}"
+        )
+        return redirect("alltransfers")
