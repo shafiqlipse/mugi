5f835578aa12518ab38fa94299b2a4b2e99ad63e
shafiq <ssekittoshafiq@gmail.com>
2025-02-12 08:37:49 +0300

    faces

--- school/views.py
+++ school/views.py
@@ -778,7 +778,7 @@ import json
 import logging
 from .models import Payment  # Use the Payment model
 
-
+callback_logger = logging.getLogger('airtel_callback')
 # def airtel_payment_callback(request):
 #     try:
 #         data = json.loads(request.body)
