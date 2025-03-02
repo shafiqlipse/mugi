15e88f0963a1da9c7afdffe50107a215f2005123
shafiq <ssekittoshafiq@gmail.com>
2025-03-02 10:53:35 +0300

    eee

--- transfers/views.py
+++ transfers/views.py
@@ -338,7 +338,7 @@ def export_tcsv(request):
     )  # Replace with your model's fields
 
     # Write data rows
-    for obj in TransferRequest.objects.all():
+    for obj in TransferRequest.objects.filter(status="approved"):
         writer.writerow(
             [
                 obj.id,
