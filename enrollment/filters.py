ae2974f6ac683a3a6d364220bb2bda3ac989028f
shafiq <ssekittoshafiq@gmail.com>
2025-03-11 12:34:02 +0300

    ssd

--- enrollment/filters.py
+++ enrollment/filters.py
@@ -12,3 +12,7 @@ class SchoolEnrollmentFilter(django_filters.FilterSet):
             "sport",
             "level",
         ]  # Add all fields you want to filter on
+    def __init__(self, *args, **kwargs):
+        super(SchoolEnrollmentFilter, self).__init__(*args, **kwargs)
+        for field_name, field in self.form.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
