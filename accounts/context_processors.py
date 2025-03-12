7a55e6229188d2685f19755ba3e7bcaf3e50a8a9
shafiq <ssekittoshafiq@gmail.com>
2025-03-12 18:08:56 +0300

    dds

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -2,11 +2,9 @@ from .models import SystemStatus
 
 def system_status(request):
     system_status = SystemStatus.objects.first()
-    closure_time = system_status.closure_start if system_status else None
-
-    print(f"DEBUG: Closure Time → {closure_time}")  # Debugging
 
     return {
         "system_closed": system_status.is_system_closed() if system_status else False,
-        "closure_time": closure_time
+        "closure_time": system_status.closure_start if system_status and system_status.closure_start else None,
+        "closure_end": system_status.closure_end if system_status and system_status.closure_end else None,
     }
