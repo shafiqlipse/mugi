4ee617ec6d0b3db37d1c03de4febb7c78686591f
shafiq <ssekittoshafiq@gmail.com>
2025-03-16 12:06:06 +0300

    sch

--- enrollment/filters.py
+++ enrollment/filters.py
@@ -1,8 +1,31 @@
 import django_filters
 from .models import SchoolEnrollment
-
+from school.models import School
+from accounts.models import Championship,Sport
+from django import forms
 
 class SchoolEnrollmentFilter(django_filters.FilterSet):
+    school = django_filters.ModelChoiceFilter(
+        queryset=School.objects.all(),
+        label="School",
+        widget=forms.Select(attrs={"class": "form-control js-example-basic-single"})
+    )
+    championship = django_filters.ModelChoiceFilter(
+        queryset=Championship.objects.all(),
+        label="Championship",
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
+    sport = django_filters.ModelChoiceFilter(
+        queryset=Sport.objects.all(),
+        label="Sport",
+        widget=forms.Select(attrs={"class": "form-control js-example-basic-single"})
+    )
+    level = django_filters.ChoiceFilter(
+        choices=[("District", "District"), ("Zone", "Zone"), ("National", "National")],
+        label="Level",
+      
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
 
     class Meta:
         model = SchoolEnrollment
@@ -12,7 +35,4 @@ class SchoolEnrollmentFilter(django_filters.FilterSet):
             "sport",
             "level",
         ]  # Add all fields you want to filter on
-    def __init__(self, *args, **kwargs):
-        super(SchoolEnrollmentFilter, self).__init__(*args, **kwargs)
-        for field_name, field in self.form.fields.items():
-            field.widget.attrs.update({"class": "form-control"})
+ 
