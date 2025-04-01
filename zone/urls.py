5e83d58763923f9a23de7641c039f2a91bd1ccbd
shafiq <ssekittoshafiq@gmail.com>
2025-04-01 11:12:37 +0300

    asd

--- zone/urls.py
+++ zone/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from debug_toolbar.toolbar import debug_toolbar_urls
 from .views import *
 
 
@@ -13,4 +12,4 @@ urlpatterns = [
     path("zonalchair/athletes/", zonalchair_athletes, name="zonalchair_athletes"),
     path("zonalchair/officials/", zonalchair_officials, name="zonalchair_officials"),
     path("zonalchair/enrollments/", zonalchair_enrollments, name="zonalchair_enrollments"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
