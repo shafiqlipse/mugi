883ccc99b88657cfaa7415caf1233f202fbecb23
shafiq <ssekittoshafiq@gmail.com>
2026-04-10 09:32:30 +0300

    cert

--- training/models.py
+++ training/models.py
@@ -16,6 +16,8 @@ class Venue(models.Model):
     name = models.CharField(max_length=50)
     season = models.ForeignKey(Season, on_delete=models.CASCADE)
     courses = models.ManyToManyField('Course', blank=True)
+    
+    # amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     status = models.CharField( choices=[("Active", "Active"), ("Inactive", "Inactive")], max_length=10, default="Active" ,null=True, blank=True)
     def __str__(self):
         return self.name
